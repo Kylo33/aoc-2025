2b3229e5993a27a32cb02b9653d215850f859015
Kylo33 <56988649+Kylo33@users.noreply.github.com>
2025-12-03 22:44:07 -0700

    add basic generic starter code

---
+++ starter.py
@@ -0,0 +1,9 @@
+from collections import Counter, defaultdict, deque
+import heapq
+
+import sys
+text = sys.stdin.read().strip()
+grid = [list(line) for line in text.split('\n')]
+
+deltas_4 = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+deltas_8 = [(1, 1), (1, 0), (1, -1), (0, 1), (0, -1), (-1, 1), (-1, 0), (-1, -1)]
