6aea396a3d88dec59f602855d919b4ecd714f6b5
Kylo33 <56988649+Kylo33@users.noreply.github.com>
2025-12-11 21:34:10 -0700

    update starter code w/ OR-Tools

--- starter.py
+++ starter.py
@@ -1,9 +1,14 @@
 from collections import Counter, defaultdict, deque
 import heapq
 
+# from ortools.init.python import init
+# from ortools.linear_solver import pywraplp
+# solver = pywraplp.Solver.CreateSolver("CBC")
+
 import sys
 text = sys.stdin.read().strip()
 grid = [list(line) for line in text.split('\n')]
+lines = text.split("\n")
 
 deltas_4 = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 deltas_8 = [(1, 1), (1, 0), (1, -1), (0, 1), (0, -1), (-1, 1), (-1, 0), (-1, -1)]
